64be7db0c7d6693606c23f6024b6c7bd82fd9058
SiddharthJainAutomate <jain.siddharth91@hotmail.com>
2022-08-28 21:24:34 +0530

    First Commit

---
+++ Vitamojo/src/main/java/com/vitamojo/Testcases/GlobalVariable.java
@@ -0,0 +1,14 @@
+package com.vitamojo.Testcases;
+/**
+ * @author siddharthj
+ * All Global varaible defined in the below class
+ */
+
+public class GlobalVariable {
+	
+	public static String URL = "https://www.vitamojo.com/";
+	public static String driverPath = "C:\\Users\\siddharthj\\Downloads\\chromedriver.exe";
+	public static String driverPath1 = "C:\\Users\\siddharthj\\Downloads\\geckodriver.exe";
+	
+
+}
---
+++ Vitamojo/src/main/java/com/vitamojo/Testcases/HomePage.java
@@ -0,0 +1,93 @@
+package com.vitamojo.Testcases;
+
+import java.io.IOException;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+import com.vitamojo.Pages.workPage;
+/**
+ * @author siddharthj
+ * To class will work as POM as well as Generate the reports as well.
+ */
+
+public class HomePage {
+	public static ExtentTest test;
+	ExtentHtmlReporter htmlReporter;
+	ExtentReports extent;
+	WebDriver driver;
+	
+	
+	@BeforeSuite
+	public void setup() {	
+		  htmlReporter = new ExtentHtmlReporter("ExtentReport_Vita_Mojo.html");
+	      extent = new ExtentReports();
+	      extent.attachReporter(htmlReporter);
+	}
+	
+	
+	@Parameters({"browser"})
+	@BeforeTest
+	public void setUpTest(String browser) {
+		System.out.println("Running Browser is " + browser);
+		if(browser.equals("chrome")) {
+			System.setProperty("webdriver.chrome.driver", GlobalVariable.driverPath);
+		    driver =new ChromeDriver();
+	        driver.manage().window().maximize();
+			driver.get(GlobalVariable.URL);
+			driver.get("https://careers.vitamojo.com/");
+		   
+		}
+		if (browser.equals("firefox")) {
+			System.setProperty("webdriver.gecko.driver", GlobalVariable.driverPath1);
+		    driver =new FirefoxDriver();   
+		}
+		 
+		
+	}
+	 
+	@Test
+	public void test1() throws IOException {
+		ExtentTest test = extent.createTest("Vita Mojo POC on", "Contact Page verification");
+        driver.manage().window().maximize();
+		driver.get(GlobalVariable.URL);
+		workPage work =new workPage(driver);
+		work.navigatCareerPage();
+		test.log(Status.PASS, "Navigated to Home Page successfully");
+		work.VerifyDestinationURL();
+		test.log(Status.PASS, "Navigated to Carrer page and verified URL successfully");
+		work.navigateCarrerSection();
+		test.log(Status.PASS, "Landed on my Dream Job");	
+	}
+	
+	
+	
+	@AfterTest
+	public void tearDownTest() {
+		//driver.close();
+		//driver.quit();
+		System.out.println("Test completed successfully");
+	}
+	
+	@AfterSuite
+	public void tearDown() {
+	    extent.flush();
+	}
+	
+	
+	
+	
+
+}
